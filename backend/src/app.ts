40642b1406b03c3271d71f7fae5a33b5d9a5a0bf
Sebastian-Debicki <ssebaa9@gmail.com>
2021-03-19 09:16:42 +0100

    add indexes for shorter import paths

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import createError from 'http-errors';
 
-import { errorHandler } from './common/helpers/error-handler';
-import { getProductsRouter } from './products/routes/get-products';
-import { createProductRouter } from './products/routes/create-product';
-import { deleteProductRouter } from './products/routes/delete-product';
-import { updateProductRouter } from './products/routes/update-product';
+import { errorHandler } from './common';
+import {
+  getProductsRouter,
+  createProductRouter,
+  deleteProductRouter,
+  updateProductRouter,
+} from './products';
 
 const app = express();
 
---
+++ backend/src/products/routes/index.ts
@@ -0,0 +1,4 @@
+export { getProductsRouter } from './get-products';
+export { createProductRouter } from './create-product';
+export { deleteProductRouter } from './delete-product';
+export { updateProductRouter } from './update-product';
