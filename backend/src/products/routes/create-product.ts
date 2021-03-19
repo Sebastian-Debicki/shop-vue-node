130906b55ea7ecb666d399620b23e3c531cee3d4
Sebastian-Debicki <ssebaa9@gmail.com>
2021-03-19 16:02:04 +0100

    add basic routes validations

--- backend/src/products/routes/create-product.ts
+++ backend/src/products/routes/create-product.ts
@@ -1,21 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import { body } from 'express-validator';
 
 import { Product } from '../../entity';
+import { validateRequest } from '../../common';
 
 const router = express.Router();
 
-router.post('/api/products', async (req, res) => {
-  const productRepo = getRepository(Product);
+router.post(
+  '/api/products',
+  [
+    body('price').not().isEmpty().withMessage('Price is required'),
+    body('description').not().isEmpty().withMessage('Description is required'),
+  ],
 
-  const product = productRepo.create({
-    price: req.body.price,
-    description: req.body.description,
-  });
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const productRepo = getRepository(Product);
 
-  await productRepo.save(product);
+    const product = productRepo.create({
+      price: req.body.price,
+      description: req.body.description,
+    });
 
-  res.status(201).send(product);
-});
+    await productRepo.save(product);
+
+    res.status(201).send(product);
+  },
+);
 
 export { router as createProductRouter };
