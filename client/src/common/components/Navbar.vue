7b6571cea72287322aa302e85347e681b946aea4
Sebastian-Debicki <ssebaa9@gmail.com>
2021-03-23 17:34:46 +0100

    add basic folder structure, configure scss

---
+++ client/src/common/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar">
+    <h4 class="navbar__brand">Shop</h4>
+    <ul>
+      <li>
+        <a>Dashboard</a>
+      </li>
+      <li>
+        <a>Products</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {},
+})
+export default class Navbar extends Vue {}
+</script>
+
+<style lang="scss">
+@use '../styles/colors.scss' as color;
+@use '../styles/fonts.scss' as font;
+
+.navbar {
+  background-color: color.$white;
+  height: 8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+
+  &__brand {
+    @include font.format-text('xlarge', 700);
+  }
+}
+</style>
