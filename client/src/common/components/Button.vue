847db4b8f1895af8d70544fb4b4ce8b2bad7c895
Sebastian-Debicki <ssebaa9@gmail.com>
2021-03-24 20:40:05 +0100

    setup scss, add basic vue components

---
+++ client/src/common/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    title: {
+      type: String,
+      requred: true,
+    },
+  },
+})
+export default class Button extends Vue {}
+</script>
+
+<style lang="scss"></style>
--- client/src/common/components/Navbar.vue
+++ client/src/common/components/Navbar.vue
@@ -1,22 +1,36 @@
 <template>
   <nav class="navbar">
-    <h4 class="navbar__brand">Shop</h4>
-    <ul>
-      <li>
-        <a>Dashboard</a>
-      </li>
-      <li>
-        <a>Products</a>
-      </li>
-    </ul>
+    <div class="navbar__wrapper">
+      <h4 class="navbar__brand">Shop-brand</h4>
+      <div class="navbar__input-wrapper">
+        <Input placeholder="Search" /><Button title="Search" />
+      </div>
+      <ul class="navbar__list">
+        <li>
+          <a>Dashboard</a>
+        </li>
+        <li>
+          <a>Products</a>
+        </li>
+        <li>
+          <a>Login</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+
 @Options({
-  components: {},
+  components: {
+    Input,
+    Button,
+  },
 })
 export default class Navbar extends Vue {}
 </script>
@@ -24,17 +38,37 @@ export default class Navbar extends Vue {}
 <style lang="scss">
 @use '../styles/colors.scss' as color;
 @use '../styles/fonts.scss' as font;
+@use '../styles/variables.scss' as var;
 
 .navbar {
   background-color: color.$white;
-  height: 8rem;
+  min-height: 8rem;
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 0 2rem;
+  justify-content: center;
+  padding: 0 3.5rem;
+  box-shadow: 0px 1px 5px 0 rgba(color.$dark-grey, 0.3);
+
+  &__wrapper {
+    width: var.$max-width;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
   &__brand {
     @include font.format-text('xlarge', 700);
+  }
+
+  &__input-wrapper {
+    width: 50%;
+    display: flex;
+  }
+
+  &__list {
+    list-style: none;
+    display: flex;
+    gap: 2rem;
   }
 }
 </style>
